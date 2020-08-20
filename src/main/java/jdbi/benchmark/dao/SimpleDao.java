99b323e3134756555186b46152f3a27f2b5e36f0
Natan Abolafya <natan.abolafya@cyxtera.com>
2020-08-20 18:45:56 +0200

    Make the DAO transactional?

--- src/main/java/jdbi/benchmark/dao/SimpleDao.java
+++ src/main/java/jdbi/benchmark/dao/SimpleDao.java
@@ -2,11 +2,12 @@ package jdbi.benchmark.dao;
 
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.transaction.Transactional;
 
 import java.util.List;
 
 @RegisterRowMapper(SimpleObjectMapper.class)
-public interface SimpleDao {
+public interface SimpleDao extends Transactional<SimpleDao> {
 
 	@SqlQuery("SELECT * FROM simple")
 	List<SimpleObject> getAll();
